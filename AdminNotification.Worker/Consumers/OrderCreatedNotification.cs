2f876a5eb8765821a3c6eb8afd78080e51b59642
wenceslao reyes <wenceslao.reyes@msw.com.mx>
2025-02-23 12:53:41 -0600

    Create masstransit worker project and OrderCreatedNotification consumer in it.

---
+++ AdminNotification.Worker/Consumers/OrderCreatedNotification.cs
@@ -0,0 +1,18 @@
+﻿using Contracts.Events;
+using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace AdminNotification.Worker.Consumers;
+
+public sealed class OrderCreatedNotification : IConsumer<OrderCreated>
+{
+    public async Task Consume(ConsumeContext<OrderCreated> context)
+    {
+        await Task.Delay(1000);
+
+        Console.WriteLine($"Listening from admin notification worker. Queue: {context.ReceiveContext.InputAddress}");
+
+        Console.WriteLine($"Listening from admin notification worker. Consuming order: {context.Message.OrderId}");
+    }
+}
